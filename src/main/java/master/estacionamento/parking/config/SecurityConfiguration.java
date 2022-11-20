509beba33eb167c6c463fa1d4849ef5eb083fe14
Cleber Barros <cleberbarrosgithub@gmail.com>
2022-11-20 16:20:25 -0300

    Atualizao com Swagger

--- src/main/java/master/estacionamento/parking/config/SecurityConfiguration.java
+++ src/main/java/master/estacionamento/parking/config/SecurityConfiguration.java
@@ -1,51 +1,51 @@
-package master.estacionamento.parking.config;
-
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    //Configurar o Login do Usuário
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("dio"))
-                .roles("USER")
-                .and()
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    //Configura a autorização
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/swagger-ui.html").permitAll()
-                .antMatchers("/swagger-ui/index.html").permitAll()
-                .antMatchers("/swagger-resources/**").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/v2/api-docs/**").permitAll()
-                .antMatchers("/").permitAll()
-                .antMatchers("/csrf").permitAll()
-                .antMatchers("/*.js").permitAll()
-                .antMatchers("/*.css").permitAll()
-                .antMatchers("/*.ico").permitAll()
-                .antMatchers("/*.png").permitAll();
-
-    }
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package master.estacionamento.parking.config;
+//
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    //Configurar o Login do Usuário
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password(passwordEncoder().encode("dio"))
+//                .roles("USER")
+//                .and()
+//                .passwordEncoder(passwordEncoder());
+//    }
+//
+//    @Override
+//    //Configura a autorização
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/swagger-ui.html").permitAll()
+//                .antMatchers("/swagger-ui/index.html").permitAll()
+//                .antMatchers("/swagger-resources/**").permitAll()
+//                .antMatchers("/webjars/**").permitAll()
+//                .antMatchers("/v2/api-docs/**").permitAll()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/csrf").permitAll()
+//                .antMatchers("/*.js").permitAll()
+//                .antMatchers("/*.css").permitAll()
+//                .antMatchers("/*.ico").permitAll()
+//                .antMatchers("/*.png").permitAll();
+//
+//    }
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
--- src/main/java/master/estacionamento/parking/config/SwaggerConfig.java
+++ src/main/java/master/estacionamento/parking/config/SwaggerConfig.java
@@ -1,37 +1,46 @@
 //package master.estacionamento.parking.config;
-//
-//import org.springframework.context.annotation.Bean;
+//import io.swagger.v3.oas.models.annotations.OpenAPI30;
+//import org.springframework.security.core.context.SecurityContext;
 //import org.springframework.stereotype.Component;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Documentation;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //
+//import java.util.Arrays;
+//
+////import org.springframework.context.annotation.Bean;
+////import org.springframework.stereotype.Component;
+////import springfox.documentation.builders.ApiInfoBuilder;
+////import springfox.documentation.builders.RequestHandlerSelectors;
+////import springfox.documentation.service.ApiInfo;
+////import springfox.documentation.service.Documentation;
+////import springfox.documentation.spi.DocumentationType;
+////import springfox.documentation.spring.web.plugins.Docket;
+////import springfox.documentation.swagger2.annotations.EnableSwagger2;
+////
 ////@Component
-////@EnableSwagger2
-//public class SwaggerConfig {
-//
-//   // @Bean
-//    public Docket getDocket(){
-//    return new Docket(DocumentationType.SWAGGER_2)
-//            .select()
-//            .apis(RequestHandlerSelectors.basePackage("master.estacionamento.parking"))
-//            .build()
-//            .apiInfo(metaData());
-//
-//    }
-//
-//    private ApiInfo metaData() {
-//        return new ApiInfoBuilder()
-//                .title("Parking REST API")
-//                .description("Spring Boot REST API for Parking")
-//                .version("1.0.0")
-//                .license("Apache License Version 2.0")
-//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-//                .build();
-//    }
-//
-//}
+////@OpenAPI30
+////public class SwaggerConfig {
+////
+////    private SecurityContext getSecurityContext() {
+////        return SecurityContext.builder()
+////                .securityReferences(Arrays.asList(basicAuthReference()))
+////    }
+////   // @Bean
+////    public Docket getDocket(){
+////    return new Docket(DocumentationType.SWAGGER_2)
+////            .select()
+////            .apis(RequestHandlerSelectors.basePackage("master.estacionamento.parking"))
+////            .build()
+////            .apiInfo(metaData());
+////
+////    }
+////
+////    private ApiInfo metaData() {
+////        return new ApiInfoBuilder()
+////                .title("Parking REST API")
+////                .description("Spring Boot REST API for Parking")
+////                .version("1.0.0")
+////                .license("Apache License Version 2.0")
+////                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+////                .build();
+////    }
+////
+////}
